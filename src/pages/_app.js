2066e1073c0b9d156377bca4778d2901ac489dae
Vincenz Herzog <herzogv88@gmail.com>
2023-04-12 16:27:05 +0200

    .

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import { Montserrat } from "next/font/google";
+
+import "@/styles/globals.css";
+import Layout from "@/components/Layout";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <style>
+        {`
+          html {
+            font-family: ${montserrat.style.fontFamily}
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
