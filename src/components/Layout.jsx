66604171d62ac220a31e3d96969af051017881f6
Vincenz Herzog <herzogv88@gmail.com>
2023-04-13 14:43:27 +0200

    Fix google font issue

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { MonteCarlo, Montserrat } from "next/font/google";
 
 import Meta from "./Meta";
 import Header from "./Header";
 
+const montserrat = Montserrat({
+  subsets: ["latin"],
+});
+
 const Layout = ({ children, title, headerTitle }) => {
   return (
     <>
-      <Meta title={title} />
-      <Header title={headerTitle} />
-      <main>{children}</main>
+      <div className={montserrat.className}>
+        <Meta title={title} />
+        <Header title={headerTitle} />
+        <main>{children}</main>
+      </div>
     </>
   );
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,9 @@
-import { Montserrat } from "next/font/google";
-
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
-
-const montserrat = Montserrat({
-  subsets: ["latin"],
-});
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-      <style>
-        {`
-          html {
-            font-family: ${montserrat.style.fontFamily}
-          }
-        `}
-      </style>
       <Component {...pageProps} />
     </Layout>
   );
